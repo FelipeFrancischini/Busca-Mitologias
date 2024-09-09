a81035bdfaf1b3a38f0ed099ad150fb71eab5beb
FelipeFrancischini <felipe.francischini09@gmail.com>
2024-09-09 17:02:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+function pesquisar(){
+    let pesquisa = document.getElementById("resultados-pesquisa");    
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    
+    
+
+for (let p of mitologias){
+    titulo = p.titulo.toLowerCase()
+    descricao = p.descricao.toLowerCase()
+
+    if(p.titulo.includes(campoPesquisa) || 
+       p.descricao.includes(campoPesquisa)){
+        resultados += `
+         <div class="item-resultado">
+                <h2>
+                    ${p.titulo}
+                </h2>
+                <p class="descricao-meta">
+                    ${p.descricao}
+                </p>
+                <a target="_blank" href= ${p.link}>
+                    Mais informações
+                </a>
+         </div>
+    `
+    }
+    if(!campoPesquisa){
+        pesquisa.innerHTML = "<h2>Por favor, escreva a mitologia que deseja conhecer!</h2>"
+        return
+    }
+    if(!resultados){
+        pesquisa.innerHTML = "<h2>Mitologia não encontrada!</h2>"
+        return
+    }
+    
+    
+}
+
+pesquisa.innerHTML = resultados
+}
+
---
+++ dados.js
@@ -0,0 +1,53 @@
+let mitologias = [
+    {
+        titulo: "Mitologia Japonesa: Os Kami e a Criação das Ilhas",
+        descricao: "A mitologia japonesa, conhecida como Shinto, gira em torno dos kami, espíritos que habitam todos os aspectos da natureza e da vida.",
+        link: "https://pt.wikipedia.org/wiki/Mitologia_japonesa",
+        deuses: ["Amaterasu", "Tsukuyomi", "Susanoo"],
+        criaturas: ["Oni", "Yokai"],
+        locais: "Monte Fuji",
+        simbolos: "Torii",
+        periodos: "Antiguidade"
+    },
+    {
+        titulo: "Mitologia Chinesa: Os Deuses Ancestrais e a Criação do Mundo",
+        descricao: "A mitologia chinesa é uma das mais antigas do mundo, com raízes que remontam a milhares de anos. Ela narra a criação do universo, a origem dos seres humanos e as aventuras de deuses e heróis.",
+        link: "https://pt.wikipedia.org/wiki/Mitologia_chinesa",
+        deuses: ["Pangu", "Nuwa", "Imperador de Jade", "Yù Huáng"],
+        criaturas: ["Dragão", "Fênix", "Qilin"],
+        locais: ["Monte Kunlun", "Jardins do Imperador de Jade"],
+        simbolos: ["Yin e Yang", "Dragão"],
+        periodos: "Antiguidade"
+    },
+    {
+        titulo: "Mitologia Romana: Os Deuses do Panteão Romano e a Fundação de Roma",
+        descricao: "A mitologia romana é fortemente influenciada pela mitologia grega, com muitos deuses romanos tendo equivalentes gregos. Ela narra a fundação de Roma e as aventuras de seus heróis.",
+        link: "https://pt.wikipedia.org/wiki/Mitologia_romana",
+        deuses: ["Júpiter", "Juno", "Netuno", "Plutão", "Ceres", "Vesta", "Marte", "Apollo", "Diana", "Minerva", "Vênus", "Vulcano"],
+        criaturas: ["Harpias", "Sátiros", "Ninfa"],
+        locais: ["Olimpo", "Hades", "Roma"],
+        simbolos: ["Águia", "Raio", "Tridente"],
+        periodos: "Antiguidade Clássica"
+    },
+    {
+        titulo: "Mitologia Nórdica: Os Deuses, Gigantes e Heróis do Norte",
+        descricao: "A mitologia nórdica, também conhecida como mitologia germânica, é um rico conjunto de lendas e crenças dos povos escandinavos, especialmente durante a Era Viking.",
+        link: "https://pt.wikipedia.org/wiki/Mitologia_nórdica",
+        deuses: ["Odin", "Thor", "Loki", "Freyr", "Freyja", "Baldr", "Heimdall", "Tyr", "Frigg"],
+        criaturas: ["Gigantes", "Elfos", "Anões", "Dragões"],
+        locais: ["Asgard", "Midgard", "Niflheim", "Muspelheim"],
+        simbolos: ["Martelo Mjölnir", "Espada Gram", "Árvore Yggdrasil"],
+        periodos: "Era Viking"
+    },
+    {
+        titulo: "Mitologia Grega: Os Deuses do Olimpo e as Aventuras dos Heróis",
+        descricao: "A mitologia grega é um dos sistemas mitológicos mais influentes da história, tendo servido de inspiração para a arte, a literatura e a filosofia ocidentais.",
+        link: "https://pt.wikipedia.org/wiki/Mitologia_grega",
+        deuses: ["Zeus", "Hera", "Poseidon", "Hades", "Demeter", "Héstia", "Ares", "Apolo", "Artemis", "Atena", "Afrodite", "Héfeitos"],
+        criaturas: ["Minotauro", "Hidra de Lerna", "Ciclope", "Harpia", "Esfinge"],
+        locais: ["Olimpo", "Hades", "Olímpia", "Delos"],
+        simbolos: ["Raio", "Arco e flecha", "Tridente"],
+        periodos: "Antiguidade Clássica"
+    }
+];
+
